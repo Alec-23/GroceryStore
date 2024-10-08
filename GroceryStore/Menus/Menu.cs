c0c7f22fcd130ee300c8a53a1366e87c911b227b
Alec Christensen <alec_christensen@hotmail.com>
2024-10-08 13:02:24 +0200

    Add project files.

---
+++ GroceryStore/Menus/Menu.cs
@@ -0,0 +1,17 @@
+﻿namespace GroceryStore.Menus;
+
+internal class Menu
+{
+    public void ShowMainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Välkommen till produktkatalogen för matbutiken Frossa");
+        Console.WriteLine("1. Lägg till en produkt");
+        Console.WriteLine("2. Visa alla produkter");
+        Console.WriteLine("3. Ta bort en produkt");
+        Console.WriteLine("4. Avsluta");
+        Console.Write("Välj ett av ovanstående alternativ (1-4):");
+
+        var input = Console.ReadLine();
+    }
+}
