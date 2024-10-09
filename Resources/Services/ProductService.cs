7dd76fe8a83e79ac937349d2f812892f1b60cdf0
Alec Christensen <alec_christensen@hotmail.com>
2024-10-09 18:20:04 +0200

    Lade till ProductService: Hanterar logik för att lägga till, ta bort och visa produkter

---
+++ Resources/Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using Resources.Models;
+
+namespace Resources.Services;
+
+public class ProductService
+{
+    private List<Products> productList = new List<Products>();
+
+    public void AddProduct(Products product)
+    {
+        productList.Add(product);
+    }
+
+    public void RemoveProduct(int productId)
+    {
+        var product = productList.Find(p => p.Id == productId);
+        if (product != null)
+        {
+            productList.Remove(product);
+        }
+    }
+
+    public List<Products> GetAllProducts()
+    {
+        return productList;
+    }
+}
