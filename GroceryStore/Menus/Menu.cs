70fe7b29ce6b081479750cb7b247a71a2cbe8933
Alec Christensen <alec_christensen@hotmail.com>
2024-10-09 12:58:27 +0200

    Gjorde om menyn

--- GroceryStore/Menus/Menu.cs
+++ GroceryStore/Menus/Menu.cs
@@ -7,10 +7,11 @@ internal class Menu
     {
         Console.Clear();
         Console.WriteLine("Välkommen till produktkatalogen för matbutiken Frossa");
-        Console.WriteLine("1. Lägg till en produkt");
-        Console.WriteLine("2. Visa alla produkter");
+        Console.WriteLine("1. Visa alla produkter");
+        Console.WriteLine("2. Lägg till en produkt");
         Console.WriteLine("3. Ta bort en produkt");
-        Console.WriteLine("4. Avsluta");
+        Console.WriteLine("4. Visa varukorg");
+        Console.WriteLine("5. Avsluta");
         Console.Write("Välj ett av ovanstående alternativ (1-4):");
 
         var input = Console.ReadLine();
